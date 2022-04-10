12347515cc404286f3bc2132171b036cfc3d3262
Daniel Bontii <daniel.bontii@amalitech.com>
2022-04-10 22:53:16 +0000

    implemented get username and logs route

--- server/src/main/java/com/timelogs/server/repositories/LogRepository.java
+++ server/src/main/java/com/timelogs/server/repositories/LogRepository.java
@@ -5,6 +5,7 @@ import java.sql.Time;
 
 import com.timelogs.server.entities.Log;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface LogRepository extends CrudRepository<Log, Long> {
@@ -13,4 +14,7 @@ public interface LogRepository extends CrudRepository<Log, Long> {
     public Log findByDateAndTimeOutAndUserId(Date date, Time time, Long userId);
 
     public Iterable<Log> findByUserId(Long userId);
+
+    @Query("SELECT new map(l.logId as logId, l.timeIn as timeIn, l.timeOut as timeOut, l.indicator as indicator, l.date as date, u.name as name) FROM Log l join User u ON l.logId = u.userId")
+    public Iterable<Object[]> findByUserNameAndLogs();
 }
