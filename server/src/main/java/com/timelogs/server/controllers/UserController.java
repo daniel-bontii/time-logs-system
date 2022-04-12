e410df377980ad480814baf56c2969b373f6ca9a
Daniel Bontii <daniel.bontii@amalitech.com>
2022-04-12 08:02:23 +0000

    implemented cors on backend

--- server/src/main/java/com/timelogs/server/controllers/UserController.java
+++ server/src/main/java/com/timelogs/server/controllers/UserController.java
@@ -8,6 +8,7 @@ import com.timelogs.server.repositories.UserRepository;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/timelogs-api/v1/users")
+@CrossOrigin(origins = "http://localhost:8081")
 public class UserController {
 
     private final UserRepository userRepository;
